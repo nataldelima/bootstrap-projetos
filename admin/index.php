4293852abba00086035b63c0cac515668dbc166e
Natal Lima <nataldelima@gmail.com>
2024-02-23 17:23:19 -0400

    Criando front-end da área administrativa

---
+++ admin/index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/style_admin.css">
+
+    <title>Painel de Controle </title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08"
+                aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+                <ul class="navbar-nav ">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Natal de Lima TI</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cadastrar Equipe</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Editar Sobre</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="#" data-bs-toggle="dropdown" aria-expanded="false">Gerenciar
+                            Equipe</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right justify-content-right">
+                    <a href="#" class="nav-link">Sair</a>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2><i class="bi bi-gear"></i> Painel de Controle</h2>
+                </div>
+                <div class="col-md-6">
+                    <p>Seu último login foi em: 12/06/2019</p>
+                </div>
+            </div>
+        </div>
+
+    </header>
+
+
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
